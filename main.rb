11d23898d19ee62c326645df07a732cba1418343
dwkns <dwkns@me.com>
2021-10-20 21:26:17 +0100

    Use linked image on page base64 in HTML

--- main.rb
+++ main.rb
@@ -15,14 +15,29 @@ class App < Sinatra::Base
     @email = params[:user_email]
     @phone = params[:user_phone]
 
+    @logo_url='https://under2.global/images/email-sig.png'
+    @logo_base64=File.read('logo_base64.txt')
+
     html =  File.read('the_html.txt')
     html.gsub!("###phone###", @phone)
     html.gsub!("###email###", @email)
     html.gsub!("###title###",@title)
     html.gsub!("###name###",@name)
+    html.gsub!("###image###",@logo_url)
 
     @html = html
     @escaped_html = CGI.escapeHTML(html)
+
+    html64 =  File.read('the_html.txt')
+    html64.gsub!("###phone###", @phone)
+    html64.gsub!("###email###", @email)
+    html64.gsub!("###title###",@title)
+    html64.gsub!("###name###",@name)
+    html64.gsub!("###image###",@logo_base64)
+
+    @html64 = html64
+    @escaped_html64 = CGI.escapeHTML(html64)
+
     erb :index
   end
 end
